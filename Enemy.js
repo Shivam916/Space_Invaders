ec93dfcfdaf430fa6efaf1b61acff2b41b35dbf7
Shivam <shivam@MacBook-Air.local>
2023-04-12 22:00:35 +0530

    project files added

---
+++ Enemy.js
@@ -0,0 +1,40 @@
+export default class Enemy{
+    constructor(x, y, imageNumber){
+        this.x = x;
+        this.y = y;
+        this.width = 44;
+        this.height = 32;
+
+        this.image = new Image();
+        // this.image.src = 'images/enemy${imageNumber}.png';
+        if(imageNumber == 1){
+            this.image.src = 'images/enemy1.png';
+        }else if(imageNumber == 2){
+            this.image.src = 'images/enemy2.png';
+        }else if(imageNumber == 3){
+            this.image.src = 'images/enemy3.png';
+        }
+    }
+
+    draw(ctx){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+    move(xVelocity, yVelocity){
+        this.x += xVelocity;
+        this.y += yVelocity;
+    }
+
+    collideWith(sprite){
+        if (
+            this.x + this.width > sprite.x &&
+            this.x < sprite.x + sprite.width &&
+            this.y + this.height > sprite.y &&
+            this.y < sprite.y + sprite.height
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
